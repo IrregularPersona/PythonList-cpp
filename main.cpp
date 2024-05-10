ede120a871ba24bb5e7ff2668bd46a1c82ed396c
IrregularPersona <irregularpersona@proton.me>
2024-05-10 20:18:21 +0700

    changed a minor thing

--- main.cpp
+++ main.cpp
@@ -2,13 +2,6 @@
 #include <string>
 #include "include/PythonList.hpp"
 
-void menu() {
-    std::cout << "1 -> int\n";
-    std::cout << "2 -> double\n";
-    std::cout << "3 -> string\n";
-    std::cout << "4 -> bool\n";
-}
-
 int main() {
 
     pythonList myList = {10, 20.5, "Hello World!"};
